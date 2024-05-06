091663868a61d8206d10d881f25320e87ff73c93
Yerim <yerim0307@gmail.com>
2024-05-06 22:44:17 +0900

    [4-1]_송예림_과제

---
+++ [4-1]_송예림_과제/coffeeProject/store/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from coffee.models import Post
+from coffee.forms import PostModelForm
+
+# Create your views here.
+def store(request):
+    return render(request, "store.html")
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, "post_list.html", {"posts" : posts})
+
+def post_detail(request, post_id) : 
+    post = Post.objects.get(id=post_id)
+    return render(request, "post_detail.html", {"post" : post})
+
+def post_update(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostModelForm(instance=post)
+        return render(request, 'form_create.html', {'form':form, 'id':id})
+    
+def post_delete(request, id):
+    post = Post.objects.get(pk=id)
+    post.delete()
+    return redirect('post_list')
