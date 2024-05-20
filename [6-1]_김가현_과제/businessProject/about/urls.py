2df66dc34d4519a25f3e36c4aa2edb7d3208a9cd
Kimgah <115142931+Kimgah@users.noreply.github.com>
2024-05-20 22:04:08 +0900

    Delete [6-1]_김가현_과제/businessProject directory

--- [6-1]_김가현_과제/businessProject/about/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from about import views
-
-urlpatterns = [
-    path("", views.about, name="about"),
-]
--- [6-1]_김가현_과제/businessProject/store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from store import views
-
-urlpatterns = [
-    path("", views.store, name="store"),
-]
